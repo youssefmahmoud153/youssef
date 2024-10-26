dadf4ebc4198a8861114191256af641d777e6133
git hub user name <your email>
2024-10-26 15:54:18 +0300

    {youssef}

---
+++ om_account_accountant/__manifest__.py
@@ -0,0 +1,42 @@
+{
+    'name': 'Odoo 18 Accounting',
+    'version': '1.0.2',
+    'category': 'Accounting',
+    'summary': 'Accounting Reports, Asset Management and Budget, Recurring Payments, '
+               'Lock Dates, Fiscal Year, Accounting Dashboard, Financial Reports, '
+               'Customer Follow up Management, Bank Statement Import',
+    'description': 'Odoo 18 Financial Reports, Asset Management and '
+                   'Budget, Financial Reports, Recurring Payments, '
+                   'Bank Statement Import, Customer Follow Up Management,'
+                   'Account Lock Date, Accounting Dashboard',
+    'live_test_url': 'https://www.youtube.com/c/OdooMates',
+    'sequence': '1',
+    'author': 'Odoo Mates, Odoo SA',
+    'maintainer': 'Odoo Mates',
+    'license': 'LGPL-3',
+    'support': 'odoomates@gmail.com',
+    'depends': [
+        'accounting_pdf_reports',
+        'om_account_asset',
+        'om_account_budget',
+        'om_fiscal_year',
+        'om_recurring_payments',
+        'om_account_daily_reports',
+        'om_account_followup',
+    ],
+    'data': [
+        'security/group.xml',
+        'views/menu.xml',
+        'views/settings.xml',
+        'views/account_group.xml',
+        'views/account_tag.xml',
+        'views/res_partner.xml',
+        'views/account_bank_statement.xml',
+        'views/payment_method.xml',
+        'views/reconciliation.xml',
+        'views/account_journal.xml',
+    ],
+    'application': True,
+    'images': ['static/description/banner_new.gif'],
+}
+
